d6384d811891a2e1c2d3377734316a3c680a0c3d
Zhong Jianxin <azuwis@gmail.com>
2020-03-18 14:55:34 +0800

    Fix purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/index.html',
+    './src/**/*.svelte'
+  ],
+  whitelistPatterns: [/svelte-/],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 const plugins =
   process.env.NODE_ENV === 'production'
-    ? ['tailwindcss', 'autoprefixer', '@fullhuman/postcss-purgecss']
+    ? ['tailwindcss', 'autoprefixer', purgecss]
     : ['tailwindcss'];
 
 module.exports = {plugins};
