e2fa188796d454286c8108aedd1f81d7b7ff7cad
Zhong Jianxin <azuwis@gmail.com>
2020-03-18 11:26:21 +0800

    Lint

--- src/App.svelte
+++ src/App.svelte
@@ -3,15 +3,19 @@ import slugify from 'slugify';
 import videos from './videos.json';
 </script>
 
+<style>
+.btn {
+  @apply inline-block bg-gray-200 rounded-full px-3 py-1 font-semibold text-gray-700 mr-1;
+}
+</style>
+
 <div class='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
   {#each videos as video}
     <div class='max-w-md rounded overflow-hidden shadow-lg m-1'>
       <img class='w-full' src='//i0.hdslb.com/bfs/archive/{video.cover}.jpg' alt='{video.title_zh}'>
       <div class='px-6 py-3'>
         <div class='font-bold text-xl mb-2'>{video.title_zh}</div>
-        <p class='text-gray-700 text-lg'>
-          {video.title_en}
-        </p>
+        <p class='text-gray-700 text-lg'>{video.title_en}</p>
       </div>
       <div class='px-6 pt-1 pb-4'>
         <a href='https://www.bilibili.com/video/{video.bilibili}' class='btn' target='_blank'>视频</a>
@@ -23,9 +27,3 @@ import videos from './videos.json';
     </div>
   {/each}
 </div>
-
-<style>
-.btn {
-  @apply inline-block bg-gray-200 rounded-full px-3 py-1 font-semibold text-gray-700 mr-1;
-}
-</style>
